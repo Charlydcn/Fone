e5064fd20d5cc2130646e73261812c595e680ae7
Charlydcn <charly.ducournau@outlook.fr>
2023-05-05 14:26:05 +0200

    (router) created router + switch case

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+use Controller\basketController;
+use Controller\homeController;
+use Controller\orderController;
+use Controller\productController;
+
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
+$ctrlBasket = new basketController();
+$ctrlHome = new homeController();
+$ctrlOrder = new orderController();
+$ctrlProduct = new productController();
+
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+
+        case "allProducts":
+
+            $ctrlProduct->getAllProducts();
+
+            break;
+    }
+}
