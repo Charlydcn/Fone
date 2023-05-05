ebdfa66f98a0448d7e2fbb024c42af94382b03cd
Charlydcn <charly.ducournau@outlook.fr>
2023-05-05 14:26:20 +0200

    (ctrl) created controllers

---
+++ controller/basketController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Controller;
+
+session_start();
+
+use Model\Connect;
+
+class basketController
+{
+    
+}
---
+++ controller/homeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Controller;
+
+session_start();
+
+use Model\Connect;
+
+class homeController
+{
+}
