8d898ee9132c92c1c5008d7dcfe8bf1a1c1faafe
Charlydcn <charly.ducournau@outlook.fr>
2023-05-05 14:26:35 +0200

    (model) created model with dbConnect method

---
+++ model/Connect.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Model;
+
+abstract class Connect {
+
+    const HOST = "localhost";
+    const DB = "fone";
+    const USER = "root";
+    const PASS = "";
+
+    public static function dbConnect() {
+        try {
+            return new \PDO(
+                    "mysql:host=" . self::HOST . ";dbname=" . self::DB . ";charset=utf8", self::USER, self::PASS);
+                    
+        } catch(\PDOException $ex) {
+            return $ex->getMessage();
+
+        }
+    }
+
+    
+
+}
