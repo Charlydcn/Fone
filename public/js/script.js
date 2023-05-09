19c6b941a5cc557d60d5aaa9e539ac3c7cec5532
Charlydcn <Charly.ducournau@outlook.fr>
2023-05-09 09:26:15 +0200

    (js)created nav script (for product list on click)

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+const productsLink = document.querySelector('nav ul:nth-of-type(2) li:nth-child(2) a');
+const productsList = document.getElementById('select_list');
+
+productsLink.addEventListener('click', function(event) {
+  // Empêche le comportement par défaut de l'élément <a>
+  event.preventDefault();
+  
+  // passe la liste en display flex
+  productsList.style.display = (productsList.style.display === 'flex') ? 'none' : 'flex';
+});
