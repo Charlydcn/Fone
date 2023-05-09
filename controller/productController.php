f7a6457b1eed93b63be2487f5c5b214e42aad2b7
Charlydcn <Charly.ducournau@outlook.fr>
2023-05-09 11:38:17 +0200

    (product ctrl)+ displaySmartphones & watches funcs

--- controller/productController.php
+++ controller/productController.php
@@ -19,4 +19,40 @@ class productController
 
         require 'view/allProducts.php';
     }
+
+    function displaySmartphones()
+    {
+        $pdo = Connect::dbConnect();
+
+        $sql = $pdo->query(
+            "SELECT id_product, product.name AS 'name', price, sale, img, product.id_category, category.name AS 'category'
+            FROM product
+            INNER JOIN category ON product.id_category = category.id_category
+            WHERE product.id_category = (
+                SELECT id_category
+                FROM category
+                WHERE NAME = 'smartphones'
+                )"
+        );
+
+        require 'view/smartphones.php';
+    }
+
+    function displaySmartwatches()
+    {
+        $pdo = Connect::dbConnect();
+
+        $sql = $pdo->query(
+            "SELECT id_product, product.name AS 'name', price, sale, img, product.id_category, category.name AS 'category'
+            FROM product
+            INNER JOIN category ON product.id_category = category.id_category
+            WHERE product.id_category = (
+                SELECT id_category
+                FROM category
+                WHERE NAME = 'smartwatches'
+                )"
+        );
+
+        require 'view/smartwatches.php';
+    }
 }
