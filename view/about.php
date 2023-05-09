8bae3a104022fb39dfa9810ac8230ee27fdd207c
Charlydcn <charly.ducournau@outlook.fr>
2023-05-09 15:13:28 +0200

    (views) created about/contact views

---
+++ view/about.php
@@ -0,0 +1,17 @@
+<?php
+
+ob_start();
+
+?>
+
+
+<?php
+
+$content = ob_get_clean();
+$title = "About";
+$secondTitle = "About";
+$css = "";
+$js = "";
+require 'template.php';
+
+?>
---
+++ view/contact.php
@@ -0,0 +1,17 @@
+<?php
+
+ob_start();
+
+?>
+
+
+<?php
+
+$content = ob_get_clean();
+$title = "Contact";
+$secondTitle = "Contact";
+$css = "";
+$js = "";
+require 'template.php';
+
+?>
