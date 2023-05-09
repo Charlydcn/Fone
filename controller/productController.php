d99d3165a8d59f2b357e87d428e9fcd77c3db32a
Charlydcn <Charly.ducournau@outlook.fr>
2023-05-09 11:18:10 +0200

    (product ctrl) created displayAllProducts func

--- controller/productController.php
+++ controller/productController.php
@@ -6,4 +6,17 @@ use Model\Connect;
 
 class productController
 {
+    function displayAllProducts()
+    {
+
+        $pdo = Connect::dbConnect();
+
+        $sql = $pdo->query(
+            "SELECT id_product, product.name AS 'name', price, sale, img, product.id_category, category.name AS 'category'
+            FROM product
+            INNER JOIN category ON product.id_category = category.id_category"
+        );
+
+        require 'view/allProducts.php';
+    }
 }
