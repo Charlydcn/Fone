d534f29f8bb3ca75edd7110ceb3e46e398d061be
Charlydcn <Charly.ducournau@outlook.fr>
2023-05-09 11:17:24 +0200

    (allProducts) displayed all products

--- view/allProducts.php
+++ view/allProducts.php
@@ -2,13 +2,34 @@
 
 ob_start();
 
+$products = $sql->fetchAll();
 ?>
+
+<h1>All products</h1>
+
+<div>
+
+    <?php
+    foreach ($products as $product) {
+    ?>
+
+        <ul>
+            <img src="public/img/<?= $product['category'] . "/" . $product['img'] ?>" alt="<?= $product['name'] ?> image">
+            <li><?= $product['name'] ?></li>
+            <li>$<?= $product['price'] ?></li>
+        </ul>
+
+    <?php } ?>
+
+</div>
 
 <?php
 
 $content = ob_get_clean();
 $title = "All products";
 $secondTitle = "All products";
+$css = "allProducts.css";
+$js = "";
 require 'template.php';
 
 ?>
