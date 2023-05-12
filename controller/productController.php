0d525a942399cbbc670a646382ca1431f4ce28e6
Charlydcn <Charly.ducournau@outlook.fr>
2023-05-12 16:54:23 +0200

    (ctrlPrdct)used model's method to get all products

--- controller/productController.php
+++ controller/productController.php
@@ -9,13 +9,7 @@ class productController
     function displayAllProducts()
     {
 
-        $pdo = Connect::dbConnect();
-
-        $sql = $pdo->query(
-            "SELECT id_product, product.name AS 'name', price, sale, img, product.id_category, category.name AS 'category'
-            FROM product
-            INNER JOIN category ON product.id_category = category.id_category"
-        );
+        $products = Connect::getProducts();
 
         require 'view/allProducts.php';
     }
