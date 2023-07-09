60b07497d1f4272f0dde8ba517161d8716e88fec
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-09 21:02:53 +0200

    (v/basket) added msg if basket's empty

--- view/basket.php
+++ view/basket.php
@@ -3,47 +3,60 @@
 ob_start();
 
 ?>
-<table>
-    <thead>
-        <tr>
-            <th colspan="2">Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
 
-        <?php
-        foreach ($_SESSION['products'] as $product) {
-        ?>
+<?php
+
+if (isset($_SESSION['products'])) {
+
+?>
+
+    <table>
+        <thead>
             <tr>
-                <td colspan="6" class="line"></td>
+                <th colspan="2">Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th></th>
             </tr>
+        </thead>
+        <tbody>
 
-            <tr>
-                <td><img src="public/img/<?= $product['category'] . "/" . $product['img'] ?>" alt="<?= $product['name'] ?> photo"></td>
-                <td><?= $product['name'] ?></td>
-                <td>$<?= $product['price'] ?></td>
-                <td>
-                    <div>
-                        <input type="button" class="remove" value="-">
-                        <p class="qtt">0</p>
-                        <input type="button" class="add" value="+">
-                    </div>
-                </td>
-                <td>$<?= $product['price'] ?></td>
-                <td>
-                    <a href="">
-                        <i class="fa-solid fa-trash"></i>
-                    </a>
-                </td>
-            </tr>
+            <?php
+            foreach ($_SESSION['products'] as $product) {
+            ?>
+                <tr>
+                    <td colspan="6" class="line"></td>
+                </tr>
 
-        <?php } ?>
-    </tbody>
-</table>
+                <tr>
+                    <td><img src="public/img/<?= $product['category'] . "/" . $product['img'] ?>" alt="<?= $product['name'] ?> photo"></td>
+                    <td><?= $product['name'] ?></td>
+                    <td>$<?= $product['price'] ?></td>
+                    <td>
+                        <div>
+                            <input type="button" class="remove" value="-">
+                            <p class="qtt">0</p>
+                            <input type="button" class="add" value="+">
+                        </div>
+                    </td>
+                    <td>$<?= $product['price'] ?></td>
+                    <td>
+                        <a href="">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </td>
+                </tr>
+
+            <?php } ?>
+        </tbody>
+    </table>
+
+<?php } else {
+    echo "<h1>There's nothing in your basket ! (yet..)</h1>";
+}
+
+?>
 
 <?php
 
