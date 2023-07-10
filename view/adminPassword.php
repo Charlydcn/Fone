5f791d969b0e1775deaa2bcb34651743d1cf0b75
Charlydcn <charly.ducournau@outlook.fr>
2023-07-10 10:59:18 +0200

    (adminPassword.php)created form for basic password

---
+++ view/adminPassword.php
@@ -0,0 +1,31 @@
+<?php
+
+ob_start();
+
+if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+
+?>
+
+<div id="adminMenu">
+    <h1>Enter password</h1>
+
+    <form action="index.php?action=handleAdminPassword" method="POST" autocomplete="off">
+
+        <input type="password" name="password" required>
+        <input type="submit" name="submit" value="Enter">
+    
+    </form>
+</div>
+
+<?php
+
+$content = ob_get_clean();
+$title = "Admin";
+$secondTitle = "Admin";
+$css = "admin.css";
+require 'template.php';
+
+?>
